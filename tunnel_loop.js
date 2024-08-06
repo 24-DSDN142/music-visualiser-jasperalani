5d15ab770b8c93df23ea32af19350561ea90b210
Jasper Alani <jasperalani@gmail.com>
2024-08-06 16:29:19 +1200

    refactors code

--- tunnel_loop.js
+++ tunnel_loop.js
@@ -1,58 +1,81 @@
-let diameter = {
-    vocal: 0,
-    drum: 0,
-    bass: 0,
-    other: 0
+let settings = {
+    vocal: {
+        diameter: 0,
+        dModifier: 1.25
+    },
+    drum: {
+        diameter: 0,
+        dModifier: 1.25
+    },
+    bass: {
+        diameter: 0,
+        dModifier: 1.25
+    },
+    other: {
+        diameter: 0,
+        dModifier: 1.25
+    },
 };
 let objects = []
 let colour = "white";
+let u = 20;
+let draw_ = true; // debug tool disable drawing objects
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-    background(20)
+    grid();
+    displayMousePosition();
+
+    // background(20)
     textFont('Arial');
     rectMode(CENTER)
     textSize(24);
 
     // todo: Make tunnel loop
 
-    // circillusion: a circle getting bigger each frame causing an illusion
+    drawExpandingCircle(0, 0, settings.vocal, vocal)
+    drawExpandingCircle(width, 0, settings.drum, drum)
+    drawExpandingCircle(0, height, settings.bass, bass)
+    drawExpandingCircle(width, height, settings.other, other)
+}
 
-    // Vocals circillusion
-    translate(width / 4, height / 4);
+function drawExpandingCircle(x, y, object, volume) {
     push();
     fill(colour);
     strokeWeight(0)
-    console.log("vocal: " + diameter.vocal)
-    if (diameter.vocal < canvasWidth + 500) {
-        circle(0, 0, diameter.vocal);
+    if (object.diameter < canvasWidth + 500) {
+        draw_debug(() => circle(x, y, object.diameter))
     } else {
-        diameter.vocal = 0;
-        if (colour === "black") {
-            colour = "white"
-        } else {
-            colour = "black"
-        }
+        object.diameter = 0;
+        colour = colour === "black" ? "white" : "black";
     }
+    draw_debug(() => object.diameter += volume * object.dModifier)
     pop();
-    diameter.vocal += vocal * 1.25
+}
 
-    // Drums circillusion
-    translate(width / 2, height / 4);
-    push();
-    fill(colour);
-    strokeWeight(0)
-    console.log("drum: " + diameter.drum)
-    if (diameter.drum < canvasWidth + 500) {
-        circle(0, 0, diameter.drum);
-    } else {
-        diameter.drum = 0;
-        if (colour === "black") {
-            colour = "white"
-        } else {
-            colour = "black"
+function draw_debug(callback, force){
+    if(draw_ || force){
+        callback()
+    }
+}
+
+function grid() {
+    background(200);
+    stroke(220);
+    strokeWeight(1);
+    for (let x = 0; x <= width; x += u) {
+        for (let y = 0; y <= height; y += u) {
+            line(x, 0, x, height);
+            line(0, y, width, y);
         }
     }
-    pop();
-    diameter.drum += drum * 1.25
+}
+
+function displayMousePosition() {
+    textFont('menlo');
+    textSize(14);
+    noStroke();
+    text("x:" + mouseX, 10, 20);
+    text("y:" + mouseY, 10, 40);
+    stroke('black'); // reset stroke
 }
